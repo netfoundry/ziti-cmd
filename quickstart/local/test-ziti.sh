9fd031c408951ef562fd04ffe23e47df0f0ac1bd
dovholuknf <46322585+dovholuknf@users.noreply.github.com>
2020-11-21 01:21:53 -0500

    wip nov21

--- quickstart/local/test-ziti.sh
+++ quickstart/local/test-ziti.sh
@@ -2,12 +2,15 @@ CURDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 ziti edge controller login "${ZITI_EDGE_API_HOSTNAME}" -u "${ZITI_USER}" -p "${ZITI_PWD}" -c "${ZITI_PKI}/${ZITI_EDGE_ROOTCA_NAME}/certs/${ZITI_EDGE_INTERMEDIATE_NAME}.cert"
 
-svc=clint-ssh-hosted-dns ihost=clint.ssh.hosted iport=22 sproto=tcp shost=wsl2-controller sport=2200 $CURDIR/make-service.sh
-svc=clint-ssh-hosted-ip ihost=192.168.15.3 iport=22 sproto=tcp shost=wsl2-controller sport=2200 $CURDIR/make-service.sh
-
-svc=clint-ssh-router-dns ihost=clint.ssh.router iport=22 to=tcp:localhost:2200 $CURDIR/make-service.sh
-svc=clint-ssh-router-ip ihost=192.168.15.4 iport=22 to=tcp:localhost:2200 $CURDIR/make-service.sh
-
+#svc=clint-ssh-hosted-dns ihost=clint.ssh.hosted iport=22 sproto=tcp shost=wsl2-controller sport=2200 $CURDIR/make-service.sh
+#svc=clint-ssh-hosted-ip ihost=192.168.15.3 iport=22 sproto=tcp shost=wsl2-controller sport=2200 $CURDIR/make-service.sh
+#
+#svc=clint-ssh-router-dns ihost=clint.ssh.router iport=22 to=tcp:localhost:2200 $CURDIR/make-service.sh
+#svc=clint-ssh-router-ip ihost=192.168.15.4 iport=22 to=tcp:localhost:2200 $CURDIR/make-service.sh
+#
+#svc=clint-ssh-router-eth0 ihost=clint.router.eth0 iport=80 to=tcp:eth0:80 $CURDIR/make-service.sh
+svc=eth0-by-ip ihost=192.168.15.15 iport=80 to=tcp:eth0:80 $CURDIR/make-service.sh
+svc=eth0-by-name ihost=eth0-by-name iport=80 to=tcp:eth0:80 $CURDIR/make-service.sh
 
 ziti edge controller delete service-policy dial-all
 ziti edge controller create service-policy dial-all Dial --service-roles '#all' --identity-roles '#all'
@@ -19,5 +22,6 @@ ziti edge controller create service-policy bind-all Bind --service-roles '#all' 
 #ziti-tunnel proxy netcatsvc:8145 -i "${ZITI_HOME}/test_identity".json > "${ZITI_HOME}/ziti-test_identity.log" 2>&1 &
 cp "${ZITI_HOME}/test_identity.jwt" /mnt/v/temp/identities/_new_id.jwt
 
+for i in {14030..14040}; do ./loop.sh; $i done
 
-
+iport=13000 svc=eth0.${iport}.ziti ihost=eth0.${iport}.ziti to=tcp:eth0:80 $CURDIR/make-service.sh
